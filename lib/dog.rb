30ae81517494f0b1a3ad0c30c7d95e86a087624f
mccleskeyc <mccleskeyc@gmail.com>
2020-11-15 15:14:28 -0500

    commit tree 4aabf86c2d57736f8b9f6c75ab6c5de1c07a11fb

---
+++ lib/dog.rb
@@ -0,0 +1,9 @@
+class Dog # adding class definition Dog
+    def bark # defines method bark 
+        puts "Woof!" # that puts "Woof!"
+    end #ends method bark   
+
+    def sit # defines method sit 
+        puts "The Dog is sitting" # that puts phrase
+    end # ends method sit
+end # ending class definition dog
---
+++ lib/person.rb
@@ -0,0 +1,9 @@
+class Person # defines class Person
+    def talk # defines method talk
+        puts "Hello World!" # puts phrase
+    end # ends method talk
+
+    def walk # defines method walk
+        puts "The Person is walking" # puts phrase
+    end # ends method walk
+end # ends class Person
